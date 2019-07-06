8f9b93f4c75a8a5e00d68c6440dcf5b569ce45f9
rakeshmadu <rakeshmadu24@gmail.com>
2019-07-06 14:34:33 +0530

    First commit

---
+++ cricketapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import get_first_cric,get_capkeep,post_fav,get_cap,get_dat,get_btw,get_right
+ #from .views import get_capkeep
+
+urlpatterns=[
+    path('cricket/',get_first_cric),
+    path('capwi/',get_capkeep),
+    path('cap/',get_cap),
+    path('fav/',post_fav),
+    path('data/',get_dat),
+    path('btw/',get_btw),
+    path('right/',get_right),
+    # path('favv/',get_fav),
+    #path('ho/',api_add_article)
+]
